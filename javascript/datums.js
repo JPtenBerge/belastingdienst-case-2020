756b0985d37142b6f726cb2c50e891182ffb3168
JP <jp@jp.nl>
2021-01-15 15:47:37 +0100

    Even wat JavaScript en datums laten zien

---
+++ javascript/datums.js
@@ -0,0 +1,21 @@
+
+
+var nu = new Date();
+
+console.log(nu.getDate());
+console.log(nu.getMonth() + 1);
+console.log(nu.getFullYear());
+
+var toen1 = new Date('2021/03/04');
+var toen2 = new Date('2021-03-04');
+console.log(toen1);
+console.log(toen2);
+
+// java heeft deze zelfde shizzle.
+
+// java 8 calendar API
+
+
+// DOM API
+
+// frameworks libraries
