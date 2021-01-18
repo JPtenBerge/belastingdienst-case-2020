4bb78537a5062161fab8f1d6171f65b6c2e8647d
JP <jp@jp.nl>
2021-01-18 15:55:45 +0100

    Block bindings laten zien en Babel werkend gekregen

---
+++ hipnieuwjs/block-bindings.js
@@ -0,0 +1,33 @@
+'use strict';
+// console.log("i voor for:", i);
+for (let i = 0; i < 5; i++) {
+  console.log("i:", i);
+}
+// console.log("i buiten for:", i);
+
+
+let x;
+x = 14;
+
+
+// x = 14;
+// var x;
+// var x;
+// var x;
+
+console.log(x);
+
+const bla = 'hoi';
+// bla = 'q';
+
+console.log('bla:', bla);
+
+const config = {
+    aantal: 14
+};
+// Object.freeze(config);
+config.aantal = 28;
+config.ietsnieuws = 'hoi';
+
+
+console.log(config);
---
+++ hipnieuwjs/src/hip-en-nieuw.js
@@ -0,0 +1,11 @@
+
+
+let arrowfunc = () => {
+    console.log('arrow hier 22222');
+};
+
+class HomeComponent {
+    constructor() {
+        console.log('hoi!');
+    }
+}
