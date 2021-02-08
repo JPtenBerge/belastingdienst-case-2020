cfad6f9e80ffa855064a0d788a7b796842f5dd05
JP <jp@jp.nl>
2021-02-08 15:07:27 +0100

    Demo van AJAX en response verwerken in pagina

---
+++ ajax-demo/backend/Controllers/PretparkController.cs
@@ -0,0 +1,30 @@
+﻿using BryanBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BryanBackend.Controllers
+{
+	[ApiController]
+	[Route("api/pretpark")]
+	public class PretparkController : ControllerBase
+	{
+		static List<PretparkModel> Pretparken = new List<PretparkModel>()
+		{
+			new PretparkModel { Id = 4, Name= "Walibi", Rating = 9 },
+			new PretparkModel { Id = 8, Name= "Efteling", Rating = 6 }
+		};
+
+		
+
+		[HttpPost]
+		public IEnumerable<PretparkModel> Post(PretparkModel newPretpark)
+		{
+			Pretparken.Add(newPretpark);
+			return Pretparken;
+		}
+	}
+}
